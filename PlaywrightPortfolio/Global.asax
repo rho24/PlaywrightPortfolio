ed83c7f0bcd5a827c831a1cc4096b7db494ec5d5
Richard Oliver <richard@rholiver.co.uk>
2014-02-12 21:30:47 +0000

    Initial sln

---
+++ PlaywrightPortfolio/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlaywrightPortfolio.MvcApplication" Language="C#" %>
---
+++ PlaywrightPortfolio/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlaywrightPortfolio.Startup))]
+namespace PlaywrightPortfolio
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
